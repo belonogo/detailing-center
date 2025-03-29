09225f18769d47d3bbd67a0c5659aa494f764b94
Pablo <74504296+belonogo@users.noreply.github.com>
2025-03-29 10:45:13 +0300

    Update bookings.py

--- main-service/app/routes/bookings.py
+++ main-service/app/routes/bookings.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from ..models import Booking, db
+from app.models import Booking  # Абсолютный импорт
+from app import db
 
 bookings_bp = Blueprint('bookings', __name__)
 
