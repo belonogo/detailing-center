9fa94c808b6ce828a73eaca586d6cf8aa7185d8b
Pablo <74504296+belonogo@users.noreply.github.com>
2025-03-29 10:44:30 +0300

    Update __init__.py

--- main-service/app/__init__.py
+++ main-service/app/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@postgres:5432/db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
     
-    with app.app_context():
-        db.create_all()
-    
-    from .routes import bookings_bp
+    from app.routes.bookings import bookings_bp  # Абсолютный импорт
     app.register_blueprint(bookings_bp, url_prefix='/bookings')
     
     return app
