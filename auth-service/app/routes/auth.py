f4676d465679e1de08b4e4d819aa622db1605e4a
Pablo <74504296+belonogo@users.noreply.github.com>
2025-03-29 10:23:55 +0300

    Update auth.py

--- auth-service/app/routes/auth.py
+++ auth-service/app/routes/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
-from ..models import User, db
+from app.models import User, db
 
 auth_bp = Blueprint('auth', __name__)
 
